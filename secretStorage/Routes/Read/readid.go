e0c3a98db395b7f68dc8a779641ebaa345eff349
Girish Dudhwal <girishdudhwal@gmail.com>
2019-02-25 13:19:19 -0600

    fixed reading from json file

--- secretStorage/Routes/Read/readid.go
+++ secretStorage/Routes/Read/readid.go
@@ -11,10 +11,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type ResponseSecret []struct {
-	ID     string `json:"id,omitempty"`
-	Token  string `json:"token,omitempty"`
-	Active bool
+type ResponseSecret struct {
+	ID     string `json:"id"`
+	Token  string `json:"token"`
+	Active bool   `json:"Active"`
 }
 
 func ReadID(w http.ResponseWriter, r *http.Request) {
@@ -29,8 +29,15 @@ func ReadID(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("Successfully Opened ", fileLoc)
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var ResponseSecret1 ResponseSecret
-	json.Unmarshal(byteValue, &ResponseSecret1)
-	fmt.Printf("%#v", ResponseSecret1)
-	//fmt.Println("This is me", ResponseSecret1.ID)
+	var ResponseSecret1 []ResponseSecret
+	//fmt.Println(string(byteValue))
+	v := json.Unmarshal([]byte(byteValue), &ResponseSecret1)
+	fmt.Println(v)
+	//fmt.Printf("%#v", ResponseSecret1)
+	fmt.Println("This is me", ResponseSecret1[0])
+	PrintObject(ResponseSecret1[0])
+}
+
+func PrintObject(r ResponseSecret) {
+	fmt.Printf("ID is :%s, Token is : %s ,Status : %t", r.ID, r.Token, r.Active)
 }
